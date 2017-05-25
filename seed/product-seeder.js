8b00862a328873d9220bf52d4f53e2e8ccbf6043
BHess2653 <GM_Dahaka@BHMac.local>
2017-05-24 23:08:48 -0400

    End of Vid 4 files

---
+++ seed/product-seeder.js
@@ -0,0 +1,57 @@
+var Product = require('../models/product');
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('localhost:27017/shopping');
+
+var products = [
+  new Product({
+    imagePath: 'https://vignette1.wikia.nocookie.net/runescape2/images/0/05/Bronze_2h_sword_detail.png',
+    title: 'Bronze 2h Sword',
+    description: 'Quality made 2h sword made from bronze material.',
+    price: 5
+  }),
+  new Product({
+    imagePath: 'https://vignette4.wikia.nocookie.net/runescape2/images/d/d8/Iron_2h_sword_detail.png',
+    title: 'Iron 2h Sword',
+    description: 'Quality made 2h sword made from Iron material.',
+    price: 8
+  }),
+  new Product({
+    imagePath: 'https://vignette1.wikia.nocookie.net/runescape2/images/5/54/Steel_2h_sword_detail.png',
+    title: 'Steel 2h Sword',
+    description: 'Quality made 2h sword made from Steel material.',
+    price: 15
+  }),
+  new Product({
+    imagePath: 'https://vignette2.wikia.nocookie.net/runescape2/images/0/0a/Mithril_2h_sword_detail.png',
+    title: 'Mithril 2h Sword',
+    description: 'Quality made 2h sword made from mithril material.',
+    price: 25
+  }),
+  new Product({
+    imagePath: 'https://vignette3.wikia.nocookie.net/runescape2/images/6/6f/Adamant_2h_sword_detail.png',
+    title: 'Adamant 2h Sword',
+    description: 'Quality made 2h sword made from adamant material.',
+    price: 60
+  }),
+  new Product({
+    imagePath: 'https://vignette2.wikia.nocookie.net/runescape2/images/c/c9/Rune_2h_sword_detail.png',
+    title: 'Rune 2h Sword',
+    description: 'Quality made 2h sword made from rune material.',
+    price: 349
+  })
+];
+var done = 0;
+for (var i = 0; i < products.length; i++) {
+  products[i].save(function(err, result) {
+    done++;
+    if (done === products.length) {
+      exit();
+    }
+  });
+}
+
+function exit() {
+  mongoose.disconnect();
+}
